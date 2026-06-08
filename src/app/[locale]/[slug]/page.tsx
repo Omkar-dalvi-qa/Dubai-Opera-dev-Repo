29b220497d2bccea047ac0f55b2a7007e17cf0bb
Omkar Dalvi <omkar.dalvi@enpointe.io>
2026-06-08 17:42:42 +0530

    clean single pipeline-2

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -1,6 +1,6 @@
 // import { notFound } from "next/navigatassasadsadion"; ////////////////////////// /////////////
 ///bdhbashfbkasbkfbaskbfkj ///////////////////////. /////////////////////
-/////////////////
+/////////////////. 1233
 import Banner from "@/components/programs/Banner";
 import { Cormorant_Garamond, Manrope, Montserrat } from "next/font/google";
 // app/[locale]/[slug]/page.tsx ////
