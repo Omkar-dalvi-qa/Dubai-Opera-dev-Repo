264f1dd135b6b2fcdbb30888093be6ec5b294405
Omkar Dalvi <omkar.dalvi@enpointe.io>
2026-06-08 15:50:46 +0530

    Jenkins dev push -91

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -1,5 +1,5 @@
 // import { notFound } from "next/navigation";
-
+///bdhbashfbkasbkfbaskbfkj
 import Banner from "@/components/programs/Banner";
 import { Cormorant_Garamond, Manrope, Montserrat } from "next/font/google";
 // app/[locale]/[slug]/page.tsx ////
